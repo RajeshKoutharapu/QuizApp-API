1ae060d117eeaf07557ed860c4f3f4b3b6783c2b
unknown <rajeshkoutharapu12345@gmail.com>
2024-08-26 22:52:41 +0530

    first commit

---
+++ QuizBackEndApplication/src/main/java/com/QuizApp/QuizBackEndApplication/QuizBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.QuizApp.QuizBackEndApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBackEndApplication.class, args);
+	}
+
+}
---
+++ QuizBackEndApplication/src/test/java/com/QuizApp/QuizBackEndApplication/QuizBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuizApp.QuizBackEndApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
